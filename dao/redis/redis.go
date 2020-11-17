c832f5c61a68544a263eee291903794fbb1e55ea
Xuenan Wang <roderick_wang@outlook.com>
2020-11-17 16:57:07 -0500

    Create files

---
+++ dao/redis/redis.go
@@ -0,0 +1,32 @@
+package redis
+
+import (
+	"fmt"
+	"web_app/settings"
+
+	"github.com/go-redis/redis"
+)
+
+// 声明一个全局的rdb变量
+var rdb *redis.Client
+
+// 初始化连接
+func Init(cfg *settings.RedisConfig) (err error) {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:         fmt.Sprintf("%s:%d", cfg.Host, cfg.Port),
+		Password:     cfg.Password, // no password set
+		DB:           cfg.DB,       // use default DB
+		PoolSize:     cfg.PoolSize,
+		MinIdleConns: cfg.MinIdleConns,
+	})
+
+	_, err = rdb.Ping().Result()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Close() {
+	_ = rdb.Close()
+}
